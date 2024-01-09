2a22297ecc5a0dfa03c8251003f4c78b05cbfc3e
MrEmmet66 <lorrenc7@gmail.com>
2024-01-09 21:51:00 +0200

    Add LibraryUser props and methods

--- PeopleClassLibrary/LibraryUser.cs
+++ PeopleClassLibrary/LibraryUser.cs
@@ -8,5 +8,29 @@ namespace PeopleClassLibrary
 {
     public class LibraryUser : Student
     {
+        public LibraryUser(string name, string surname, DateTime birthDate, int course, string group, string faculty, string university, int libraryCardId, DateTime dateOfEnrollment, int monthlyDeposit) 
+            : base(name, surname, birthDate, course, group, faculty, university)
+        {
+            LibraryCardId = libraryCardId;
+            DateOfEnrollment = dateOfEnrollment;
+            MonthlyDeposit = monthlyDeposit;
+        }
+        public LibraryUser() { }
+
+        public LibraryUser(LibraryUser libraryUser) : this(libraryUser.Name, libraryUser.Surname, libraryUser.BirthDate, libraryUser.Course, libraryUser.Group, libraryUser.Faculty, libraryUser.University, libraryUser.LibraryCardId, libraryUser.DateOfEnrollment, libraryUser.MonthlyDeposit) { }
+
+        public int LibraryCardId { get; set; }
+        public DateTime DateOfEnrollment { get; set; }
+        public int MonthlyDeposit { get; set; }
+
+        public override string ShowInfo()
+        {
+            string baseInfo = base.ShowInfo();
+            StringBuilder sb = new StringBuilder(baseInfo);
+            sb.AppendLine($"\nНомер читацького квитка: {LibraryCardId}");
+            sb.AppendLine($"Дата вступу: {DateOfEnrollment}");
+            sb.AppendLine($"Щомісячний внесок: {MonthlyDeposit}");
+            return sb.ToString();
+        }
     }
 }
