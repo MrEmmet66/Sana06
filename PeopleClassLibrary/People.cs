982e9e2e549133cfc0d2020081de2e16451000c5
MrEmmet66 <lorrenc7@gmail.com>
2024-01-09 18:27:21 +0200

    Added fields and constructor to People class

--- PeopleClassLibrary/People.cs
+++ PeopleClassLibrary/People.cs
@@ -2,6 +2,20 @@
 {
     public class People
     {
+        public People(string name, string surname, DateTime birthDate)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = birthDate;
+        }
+        public People() { }
+
+        public People(People people) : this(people.Name, people.Surname, people.BirthDate) { }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+
 
     }
 }
